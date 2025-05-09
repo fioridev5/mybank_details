c36ccdcbeef160804f1baea7b078a59c50db1c2d
Fiori Dev <fioridev5@gmail.com>
2025-05-09 20:56:22 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/sap/mybankdetailsa/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/sap/mybankdetailsa/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
